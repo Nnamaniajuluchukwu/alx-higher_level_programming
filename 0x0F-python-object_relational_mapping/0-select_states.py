bbd4d91be5f4b5216c30ba7f12956512a0f1a790
Nnamani Joel Ajuluchukwu <113372172+Nnamaniajuluchukwu@users.noreply.github.com>
2023-04-19 22:45:02 +0100

    Create 0-select_states.py

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Import modules'''
+import sys
+import MySQLdb
+
+
+def main(argv):
+    """ Access database and print all the states. """
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
